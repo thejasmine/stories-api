0ea26ef92270d22affd332089f8825c769948239
Jasmine Huang <tinjasminee@gmail.com>
2020-05-30 11:22:41 +0800

    get data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Story.destroy_all
+
+10.times do
+  Story.create!(name:Faker::TvShows::GameOfThrones.character,
+                text:Faker::TvShows::GameOfThrones.quote,
+                author:Faker::Name.name,
+                votes:[1,2,3,4,5].sample)
+end
